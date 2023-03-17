c1f48a2b31a97e81e22925495ad858b3d4a80566
Victor Manalac <manalac.victor17@gmail.com>
2023-03-17 15:46:30 +0800

    Added a function to play a single round of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // Step 1: Variable for player choice
-let playerChoice = 'rock'
 
 // Step 2: Function for Computer Choice
 function getComputerChoice() {
   let computerOptions = ['rock', 'paper', 'scissors']
-  let computerSelect =
+  let computerPick =
     computerOptions[Math.floor(Math.random() * computerOptions.length)]
-  console.log(computerSelect)
-  return computerSelect
+  return computerPick
 }
 
-getComputerChoice()
+let playerChoice = 'rock'
+let computerSelection = getComputerChoice()
+
+// Step 3: Play a single round of RPS
+function playRound(playerChoice, computerSelection) {
+  if (playerChoice === computerSelection) {
+    return 'The result is a tie.'
+  } else if (computerSelection === 'paper') {
+    return `You lose. ${computerSelection} beats ${playerChoice}`
+  } else if (computerSelection === 'scissors') {
+    return `You win! ${playerChoice} beats ${computerSelection}`
+  } else {
+    return 'No round was played'
+  }
+}
+
+console.log(playRound(playerChoice, computerSelection))
